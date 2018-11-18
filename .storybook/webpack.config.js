8332c30cf16d137b31f69df7a8d1ad9a8f25a86b
howdy39 <tatsuya.howdy@gmail.com>
2018-11-18 23:32:26 +0900

    add: import font file

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,6 +2,14 @@ const path = require("path");
 const rootPath = path.resolve(__dirname, '../');
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(otf|eot|svg|ttf|woff|woff2)(\?.+)?$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
   resolve: {
     extensions: ['.vue'],
     alias: {
